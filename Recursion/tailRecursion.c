20434d85e33960bc8cd7f3b12c332fa3dc85ad28
sushil gyawali <63179543+Sushil787@users.noreply.github.com>
2022-01-10 23:50:31 -0800

    Update tailRecursion.c

--- Recursion/tailRecursion.c
+++ Recursion/tailRecursion.c
@@ -1,13 +1,15 @@
-#include<stdio.h>
- void fun(int n){
-if(n==0)
-return;
+#include <stdio.h>
+void fun(int n)
+{
+    if (n > 0)
+    {
+        printf("%d", n);
+        fun(n-1);
+    }
+}
 
-fun(n-1);
-printf("%d",n);
- }
- int main(){
-     fun(3);
-     return 0;
- }
- 
+int main ()
+{
+    fun(3);
+    return 0;
+}
